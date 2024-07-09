e3fbd8846a2dd5905bb5c4ef8a4a7bd2929e2d78
한혜주 <hyejustudy@gmail.com>
2024-07-09 13:01:52 +0900

    Title: 청소당번 정하기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240709/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,10 @@
+room, hallway, toilet = 0, 0, 0
+n = int(input())
+for i in range(1, n):
+    if i % 12 == 0:
+        toilet += 1
+    elif i % 3 == 0:
+        hallway += 1
+    elif i % 2 == 0:
+        room += 1
+print(room, hallway, toilet)

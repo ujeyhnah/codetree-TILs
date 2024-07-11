8bf2e8fc6b58423b060add39f0540dc191f3f053
한혜주 <hyejustudy@gmail.com>
2024-07-11 18:45:58 +0900

    Title: 별표 출력하기 12, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240711/별표 출력하기 12/print-star-12.py
+++ 240711/별표 출력하기 12/print-star-12.py
@@ -1,17 +1,10 @@
 n = int(input())
-if n % 2 == 1 and n != 1:
-    for i in range(n-1):
-        for j in range(n):
-            if i % 2 == 1 and j % 2 == 0:
-                print(' ', end=' ')
-            else:
-                print('*',end = ' ')
-        print()
-else:
-    for i in range(n):
-        for j in range(n):
-            if i % 2 == 1 and j % 2 == 0:
-                print(' ', end=' ')
-            else:
-                print('*',end = ' ')
+for i in range(n):
+    if i == 0:
+        print('* ' * n)
+    else:
+        for j in range(i):
+            print('  ', end='')
+        for j in range(n - i):
+            print('* ', end='')
         print()

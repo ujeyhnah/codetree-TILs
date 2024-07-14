cb91d7534684a1d0cb7ba85c611756ee50e6aa27
한혜주 <hyejustudy@gmail.com>
2024-07-14 15:47:09 +0900

    Title: 그 계절, 그 날, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240714/그 계절, 그 날/that-season-that-day.py
+++ 240714/그 계절, 그 날/that-season-that-day.py
@@ -7,6 +7,7 @@ def whether(m):
         return 'Fall'
     elif m == 12 or m == 1 or m ==2:
         return 'Winter'
+    return -1
 
 def func(y, m, d):
     if (y % 4 == 0) or (y % 4 == 0 and y % 100 == 0 and y % 400 == 0):
@@ -20,7 +21,7 @@ def func(y, m, d):
             if 1 <= d <= 29:
                 return whether(m)
         else:
-            return -1
+            return '-1'
     else:
         if m in [1, 3, 5, 7, 8, 10, 12]:
             if 1 <= d <= 31:
@@ -32,7 +33,8 @@ def func(y, m, d):
             if 1 <= d <= 28:
                 return whether(m)
         else:
-            return -1
+            return '-1'
+    return -1
 
 Y, M, D = map(int, input().split())
 print(func(Y,M,D))

0d99638fb7d0433cd05da60815d849952bc362fa
한혜주 <hyejustudy@gmail.com>
2024-07-14 16:13:46 +0900

    Title: 그 계절, 그 날, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240714/그 계절, 그 날/that-season-that-day.py
+++ 240714/그 계절, 그 날/that-season-that-day.py
@@ -1,47 +1,36 @@
-def whether(m):
-    if 3 <= m <= 5:
-        return 'Spring'
-    elif 6 <= m <= 8:
-        return 'Summer'
-    elif 9 <= m <= 11:
-        return 'Fall'
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
     else:
-        return 'Winter'
+        return False
 
-def func(y, m, d):
-    if (y % 4 == 0) or (y % 100 == 0 and y % 400 == 0):
-        if m == 2:
-            if 1 <= d <= 29:
-                return whether(m)
-            else:
-                return -1
-        elif m in [1, 3, 5, 7, 8, 10, 12]:
-            if 1 <= d <= 31:
-                return whether(m)
-            else:
-                return -1
-        else:
-            if 1 <= d <= 30:
-                return whether(m)
-            else:
-                return -1
-    else:
-        if m == 2:
-            if 1 <= d <= 28:
-                return whether(m)
-            else:
-                return -1
-        elif m in [1, 3, 5, 7, 8, 10, 12]:
-            if 1 <= d <= 31:
-                return whether(m)
-            else:
-                return -1
-        else:
-            if 1 <= d <= 30:
-                return whether(m)
-            else:
-                return -1
-    return -1
+def is_valid_date(year, month, day):
+    days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if is_leap_year(year):
+        days_in_month[1] = 29
+
+    return True
+
+def get_season(month):
+    if month in [3, 4, 5]:
+        return "Spring"
+    elif month in [6, 7, 8]:
+        return "Summer"
+    elif month in [9, 10, 11]:
+        return "Fall"
+    elif month in [12, 1, 2]:
+        return "Winter"
 
 Y, M, D = map(int, input().split())
-print(func(Y, M, D))
+
+if is_valid_date(Y,M,D):
+    print(get_season(M))
+else:
+    print(-1)

74d99e2cf6e6456d8b55b066c439e6b1c3455062
한혜주 <hyejustudy@gmail.com>
2024-07-14 15:57:58 +0900

    Title: 그 계절, 그 날, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240714/그 계절, 그 날/that-season-that-day.py
+++ 240714/그 계절, 그 날/that-season-that-day.py
@@ -5,36 +5,39 @@ def whether(m):
         return 'Summer'
     elif 9<=m<=11:
         return 'Fall'
-    elif m == 12 or m == 1 or m == 2:
+    elif m == 12 or 1<=m<=2:
         return 'Winter'
-    return '-1'
+    else:
+        return -1
+def chk(m, d):
+    if m in [1, 3, 5, 7, 8, 10, 12]:
+        if 1 <= d <= 31:
+            return whether(m)
+        else:
+            return -1
+    elif m in [4, 6, 9, 10]:
+        if 1 <= d <= 30:
+            return whether(m)
+        else:
+            return -1
+    return -1
+
 
 def func(y, m, d):
-    if (y % 4 == 0) or (y % 4 == 0 and y % 100 == 0 and y % 400 == 0):
-        if m in [1, 3, 5, 7, 8, 10, 12]:
-            if 1 <= d <= 31:
-                return whether(m)
-        elif m in [4, 6, 9, 11]:
-            if 1 <= d <= 30:
-                return whether(m)
-        elif m == 2:
+    if y % 4 == 0 or (y % 100 == 0 and y % 400 == 0):
+        if m == 2:
             if 1 <= d <= 29:
                 return whether(m)
         else:
-            return '-1'
+            return chk(m, d)
     else:
-        if m in [1, 3, 5, 7, 8, 10, 12]:
-            if 1 <= d <= 31:
-                return whether(m)
-        elif m in [4, 6, 9, 11]:
-            if 1 <= d <= 30:
-                return whether(m)
-        elif m == 2:
+        if m == 2:
             if 1 <= d <= 28:
                 return whether(m)
-        else:
-            return '-1'
-    return '-1'
+            else:
+                return chk(m, d)
+    return -1
+
 
 Y, M, D = map(int, input().split())
-print(func(Y,M,D))
+print(func(Y, M, D))

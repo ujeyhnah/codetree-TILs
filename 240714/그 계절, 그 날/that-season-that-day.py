0ee6ac68d448160a6c1a271a5d3309dd437bc16f
한혜주 <hyejustudy@gmail.com>
2024-07-14 15:47:32 +0900

    Title: 그 계절, 그 날, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240714/그 계절, 그 날/that-season-that-day.py
+++ 240714/그 계절, 그 날/that-season-that-day.py
@@ -21,7 +21,7 @@ def func(y, m, d):
             if 1 <= d <= 29:
                 return whether(m)
         else:
-            return '-1'
+            return -1
     else:
         if m in [1, 3, 5, 7, 8, 10, 12]:
             if 1 <= d <= 31:
@@ -33,7 +33,7 @@ def func(y, m, d):
             if 1 <= d <= 28:
                 return whether(m)
         else:
-            return '-1'
+            return -1
     return -1
 
 Y, M, D = map(int, input().split())

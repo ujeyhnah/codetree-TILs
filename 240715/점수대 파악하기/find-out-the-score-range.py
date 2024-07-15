a674e9a22b529a943413bd184329ed76b2358471
한혜주 <hyejustudy@gmail.com>
2024-07-15 13:37:01 +0900

    Title: 점수대 파악하기, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240715/점수대 파악하기/find-out-the-score-range.py
+++ 240715/점수대 파악하기/find-out-the-score-range.py
@@ -4,6 +4,7 @@ for sc in arr:
     if sc == 0:
         break
     else:
-        cntrr[sc//10-1] += 1
+        if sc >= 10:
+            cntrr[sc//10-1] += 1
 for i in range(len(cntrr)-1,-1, -1):
     print(f'{(i+1)*10} - {cntrr[i]}')

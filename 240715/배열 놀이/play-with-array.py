6ccd317c1e31def18688d498f7b830dbd527ab00
한혜주 <hyejustudy@gmail.com>
2024-07-15 17:27:32 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240715/배열 놀이/play-with-array.py
+++ 240715/배열 놀이/play-with-array.py
@@ -5,6 +5,6 @@ for i in range(q):
     if co[0] == 1:
         print(arr[co[1]-1])
     elif co[0] == 2:
-        print(arr.index(co[1], 1)+1 if co[1] in arr else 0)
+        print(arr.index(co[1], 1)+1 if co[1] in arr else '0')
     elif co[0] == 3:
         print(*arr[co[1]-1:co[2]])

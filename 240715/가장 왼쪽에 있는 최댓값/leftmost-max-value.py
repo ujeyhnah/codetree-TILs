187905d4ac17dec073933d00b4d7c123be4d5628
한혜주 <hyejustudy@gmail.com>
2024-07-15 14:33:18 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240715/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240715/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -2,9 +2,9 @@ n = int(input())
 arr = list(map(int, input().split()))
 m = min(arr)
 while len(arr) > 0:
-    x = arr.index(max(arr), 1)
-    print(x+1, end=' ')
-    arr = arr[:x]
     if max(arr) == m:
         print(1)
         break
+    x = arr.index(max(arr), 1)
+    print(x+1, end=' ')
+    arr = arr[:x]

a1bb7cdafdb4ee3438316bcbade14e31ce5778cd
한혜주 <hyejustudy@gmail.com>
2024-07-16 16:51:19 +0900

    Title: 문자 제거하기, Time: 124ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240716/문자 제거하기/del-char.py
+++ 240716/문자 제거하기/del-char.py
@@ -4,6 +4,8 @@ while len(s) > 1:
     x = int(input())
     if len(s) >= x:
         s.pop(x)
+        print(''.join(s))
     else:
         s.pop(-1)
-    print(''.join(s))
+        print(''.join(s))
+        break

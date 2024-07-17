39e922eb99d91c469752197132e25c8d266a622e
한혜주 <hyejustudy@gmail.com>
2024-07-17 17:19:36 +0900

    Title: 알파벳 출력하기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240717/알파벳 출력하기/print-alphabet.py
@@ -0,0 +1,12 @@
+n = int(input())
+idx = 0
+for i in range(n):
+    for j in range(i+1):
+        ch = chr(ord('A')+idx)
+        if 'A' <= ch <= 'Z':
+            print(chr(ord('A')+idx),end='')
+            idx += 1
+        else:
+            idx = 0
+            print(chr(ord('A')+idx),end='')
+    print()

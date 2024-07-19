a797f4ec53c4382d0a4b4e694e237b8a2293e723
한혜주 <hyejustudy@gmail.com>
2024-07-19 18:47:34 +0900

    Title: 두 정수에 대한 연산값, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240719/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 240719/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -1,5 +1,5 @@
 def func(a, b):
-    return min(a,b)*2, max(a,b)+25
+    return max(a,b)+25, min(a,b)*2
 a, b = map(int, input().split())
 res1, res2 = func(a, b)
-print(res1, res2)
+print(min(res1,res2), max(res1, res2))

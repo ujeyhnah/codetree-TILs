f062c1cdd499ffa63b2fb039831be8b99cdf5d95
한혜주 <hyejustudy@gmail.com>
2024-07-19 18:41:48 +0900

    Title: palindrome 여부 판단하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240719/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
+++ 240719/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
@@ -1,7 +1,7 @@
 def strverse(s):
-    result = list(s.split())
-    result.reverse()
-    return ''.join(result)
-a = input()
+    s.reverse()
+    return ''.join(s)
+a = input().split()
 x = strverse(a)
+a = ''.join(a)
 print('Yes' if a == x else 'No' )

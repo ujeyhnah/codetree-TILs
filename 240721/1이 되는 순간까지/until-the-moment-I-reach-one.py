e37cdb1d945f5ce8347cb2ed9b31c19605d6790b
한혜주 <hyejustudy@gmail.com>
2024-07-21 01:55:14 +0900

    Title: 1이 되는 순간까지, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240721/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ 240721/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -1,14 +1,9 @@
-cnt = 0
 def func(n):
-    global cnt
     if n == 1:
-        return n
+        return 0
     if n % 2 == 0:
-        cnt += 1
-        return func(n//2)
+        return func(n//2) + 1
     else:
-        cnt += 1
-        return func(n//3)
+        return func(n//3) + 1
 n = int(input())
-func(n)
-print(cnt)
+print(func(n))

1cf7b7f100bef0842318c9be04b586c2a535af29
한혜주 <hyejustudy@gmail.com>
2024-07-21 21:11:54 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240721/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240721/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -1,3 +1,9 @@
 n = int(input())
 nrr = list(map(int, input().split()))
-print(max(nrr))
+
+def func(n):
+    if n == 0:
+        return nrr[0]
+    return max(func(n-1), nrr[n])
+
+print(func(n-1))

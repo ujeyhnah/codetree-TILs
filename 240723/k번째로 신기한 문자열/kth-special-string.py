3c004b46372eb1e9be7ca372402391918fab2c02
한혜주 <hyejustudy@gmail.com>
2024-07-23 11:02:06 +0900

    Title: k번째로 신기한 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240723/k번째로 신기한 문자열/kth-special-string.py
+++ 240723/k번째로 신기한 문자열/kth-special-string.py
@@ -3,7 +3,10 @@ n, k = int(n), int(k)
 srr = []
 for _ in range(n):
     s = input()
-    if t in s:
+    tmp = ''
+    for i in range(len(t)):
+        tmp += s[i]
+    if t == tmp:
         srr.append(s)
 srr.sort()
 print(srr[k-1])

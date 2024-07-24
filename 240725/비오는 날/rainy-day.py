ad274bc143c7b15c858cc7efde9ce44ae09d861b
한혜주 <hyejustudy@gmail.com>
2024-07-25 04:51:41 +0900

    Title: 비오는 날, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240725/비오는 날/rainy-day.py
+++ 240725/비오는 날/rainy-day.py
@@ -14,6 +14,13 @@ for i in range(n):
     if whethers[i].whe == 'Rain':
         rains.append(whethers[i])
 for i in range(len(rains)-1):
+    if int(rains[i].day[:4]) == int(rains[i].day[:4]):
+        if int(rains[i].day[5:7])==int(rains[i].day[5:7]):
+            if int(rains[i].day[8:10]) > int(rains[i+1].day[8:10]):
+                rains[i], rains[i+1] = rains[i+1], rains[i]
+        if int(rains[i].day[5:7]) > int(rains[i+1].day[5:7]):
+            rains[i], rains[i+1] = rains[i+1], rains[i]
     if int(rains[i].day[:4]) > int(rains[i+1].day[:4]):
         rains[i], rains[i+1] = rains[i+1], rains[i]
+
 print(rains[0].day, rains[0].week, rains[0].whe)

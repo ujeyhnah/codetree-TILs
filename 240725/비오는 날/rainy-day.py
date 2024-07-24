aa447c6a3a352f29829acc377258db7de73ea621
한혜주 <hyejustudy@gmail.com>
2024-07-25 04:45:21 +0900

    Title: 비오는 날, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240725/비오는 날/rainy-day.py
@@ -0,0 +1,19 @@
+class Whether:
+    def __init__(self, day, week, whe):
+        self.day = day
+        self.week = week
+        self.whe = whe
+
+whethers = []
+n = int(input())
+for _ in range(n):
+    x = input().split()
+    whethers.append(Whether(x[0],x[1],x[2]))
+rains = []
+for i in range(n):
+    if whethers[i].whe == 'Rain':
+        rains.append(whethers[i])
+for i in range(len(rains)-1):
+    if int(rains[i].day[:4]) > int(rains[i+1].day[:4]):
+        rains[i], rains[i+1] = rains[i+1], rains[i]
+print(rains[0].day, rains[0].week, rains[0].whe)

7c4f018185f6d7a00fed9798424ac6614385e5e8
한혜주 <hyejustudy@gmail.com>
2024-07-29 15:32:51 +0900

    Title: 원점으로부터의 거리, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 240729/원점으로부터의 거리/distance-from-origin.py
+++ 240729/원점으로부터의 거리/distance-from-origin.py
@@ -8,6 +8,6 @@ pos = []
 for i in range(n):
     x = input().split()
     pos.append(Pos(int(x[0]), int(x[1]), i+1))
-pos.sort(key = lambda x: -(abs(0-x.x) - abs(0-x.y)))
+pos.sort(key = lambda x: abs(0-x.x) + abs(0-x.y))
 for po in pos:
     print(po.idx)

7c4588747ea50a8a222fbf3a1892ba2678127be1
한혜주 <hyejustudy@gmail.com>
2024-07-29 18:18:06 +0900

    Title: Date to Date, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240729/Date to Date/date-to-date.py
+++ 240729/Date to Date/date-to-date.py
@@ -2,6 +2,9 @@ m1, d1, m2, d2 = map(int, input().split())
 days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 result = 0
 for m in range(m1, m2+1):
+    if m1 == m2 and d1 == d2:
+        result = 1
+        break
     if m == m1:
         result = result + days[m-1] - d1+1
     elif m == m2:

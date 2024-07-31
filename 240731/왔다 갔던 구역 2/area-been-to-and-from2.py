2872cbc0a689f3fa1f5293fbed8ece73fd5a3566
한혜주 <hyejustudy@gmail.com>
2024-07-31 14:42:39 +0900

    Title: 왔다 갔던 구역 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240731/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240731/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -3,9 +3,9 @@ nrr = [0] * 1001
 for _ in range(n):
     com = input().split()
     if com[1] == 'L':
-        for i in range(int(com[0])+100):
-            nrr[i] += 1
+        for i in range(int(com[0])):
+            nrr[i+100] += 1
     elif com[1] == 'R':
-        for i in range(int(com[0])+100, -1, -1):
-            nrr[i] += 1
+        for i in range(int(com[0]), -1, -1):
+            nrr[i+100] += 1
 print(max(nrr))

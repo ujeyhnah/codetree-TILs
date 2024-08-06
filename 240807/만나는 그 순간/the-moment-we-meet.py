be43ee70f56e8dcfd2bfa8e1cf58952be34cd3ed
한혜주 <hyejustudy@gmail.com>
2024-08-07 07:41:57 +0900

    Title: 만나는 그 순간, Time: 311ms, Memory: 41MB, Status: Passed - CodeTree

--- 240807/만나는 그 순간/the-moment-we-meet.py
+++ 240807/만나는 그 순간/the-moment-we-meet.py
@@ -1,6 +1,6 @@
 n, m = map(int, input().split())
 end_a, end_b = 0, 0
-arr = [0] * 100001
+arr = [0] * 1000001
 idx = 1
 for _ in range(n):
     d, t = input().split()
@@ -14,7 +14,7 @@ for _ in range(n):
         for i in range(idx, idx+t+1):
             arr[i] = arr[i-1]+1
         idx = i
-brr = [0] * 100001
+brr = [0] * 1000001
 idx = 1
 for _ in range(m):
     d, t = input().split()

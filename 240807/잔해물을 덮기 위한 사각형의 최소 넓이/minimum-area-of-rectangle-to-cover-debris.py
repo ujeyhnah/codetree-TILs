e83a1111b559e7001dc15e4952008601d21ee697
한혜주 <hyejustudy@gmail.com>
2024-08-07 04:48:29 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240807/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240807/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,13 +1,16 @@
-x1, y1, x2, y2 = map(int, input().split())
-x_1, y_1, x_2, y_2 = map(int, input().split())
+x1_1, y1_1, x2_1, y2_1 = map(int, input().split())
+x1_2, y1_2, x2_2, y2_2 = map(int, input().split())
 
-rect = (x2 - x1) * (y2 - y1) # 첫 번째 직사각형 넓이
-ix1, iy1, ix2, iy2 = max(x1, x2), max(y1, y2), min(x_1, x_2), min(y_1, y_2) # 교집합 좌표
+rect = (x2_1 - x1_1) * (y2_1 - y1_1) # 첫 번째 직사각형 넓이
+ix1, iy1, ix2, iy2 = max(x1_1, x1_2), max(y1_1, y1_2), min(x2_1, x2_2),min(y2_1, y2_2) # 교집합 좌표
 
 # 교집합 넓이
 if ix1 < ix2 and iy1 < iy2:
     irect = (ix2 - ix1) * (iy2 - iy1)
 else:
     irect = 0
-
-print(rect - irect)
+result = rect - irect
+if result < rect:
+    print(rect)
+else:
+    print(result)

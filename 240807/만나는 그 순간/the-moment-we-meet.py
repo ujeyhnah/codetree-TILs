b11aaf0b1eb68c2349eadcecf1022cfb95465e42
한혜주 <hyejustudy@gmail.com>
2024-08-07 07:40:46 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240807/만나는 그 순간/the-moment-we-meet.py
+++ 240807/만나는 그 순간/the-moment-we-meet.py
@@ -1,9 +1,11 @@
 n, m = map(int, input().split())
+end_a, end_b = 0, 0
 arr = [0] * 2001
 idx = 1
 for _ in range(n):
     d, t = input().split()
     t = int(t)
+    end_a += t
     if d == 'L':
         for i in range(idx, idx+t+1):
             arr[i] = arr[i-1]-1
@@ -17,6 +19,7 @@ idx = 1
 for _ in range(m):
     d, t = input().split()
     t = int(t)
+    end_b += t
     if d == 'L':
         for i in range(idx, idx+t+1):
             brr[i] = brr[i-1]-1
@@ -27,7 +30,7 @@ for _ in range(m):
         idx = i
 
 result = -1
-for i in range(1, len(arr)):
+for i in range(1, min(end_a, end_b)):
     if arr[i] == brr[i]:
         result = i
         break

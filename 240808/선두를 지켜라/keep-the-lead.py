503f08fedc4456ebf606337b64f634094770b849
한혜주 <hyejustudy@gmail.com>
2024-08-08 15:07:00 +0900

    Title: 선두를 지켜라, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240808/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+arr = [0] * 1001
+brr = [0] * 1001
+idx = 0
+for _ in range(n):
+    v, t = map(int, input().split())
+    for i in range(idx+1, idx+t+1):
+        arr[i] = arr[i-1] + (v*t)/t
+    idx = i
+idx = 0
+for _ in range(m):
+    v, t = map(int, input().split())
+    for i in range(idx+1, idx+t+1):
+        brr[i] = brr[i-1] + (v*t)/t
+    idx = i
+cnt = [0]
+for t in range(idx):
+    if arr[t] > brr[t]:
+        if cnt[-1] != 'a':
+            cnt.append('a')
+    else:
+        if cnt[-1] != 'b':
+            cnt.append('b')
+print(len(cnt)-2)

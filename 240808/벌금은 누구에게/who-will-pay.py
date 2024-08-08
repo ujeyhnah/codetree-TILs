2f35a66cede6dd7be57dbc8df801e088e47ce02e
한혜주 <hyejustudy@gmail.com>
2024-08-08 14:39:02 +0900

    Title: 벌금은 누구에게, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

--- 240808/벌금은 누구에게/who-will-pay.py
+++ 240808/벌금은 누구에게/who-will-pay.py
@@ -1,11 +1,10 @@
 n, m, k = map(int, input().split())
 arr = [0] * (n+1)
+result = -1
 for _ in range(m):
     x = int(input())
     arr[x] += 1
-result = -1
-for i in range(len(arr)):
-    if arr[i] >= k:
-        result = i
+    if k in arr:
+        result = arr.index(k)
         break
 print(result)

a05d5baf7b07834f8f9c44689c45dc978842fb94
한혜주 <hyejustudy@gmail.com>
2024-08-08 16:34:57 +0900

    Title: 좌우로 움직이는 로봇, Time: 202ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240808/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240808/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -12,6 +12,8 @@ for _ in range(n):
         for i in range(idx+1, idx+t+1):
             arr[i] = arr[i-1] + 1
     idx = i
+
+arr[idx+1:] = [arr[idx]] * (len(arr) - idx)
 idx = 0
 for _ in range(m):
     t, d = input().split()
@@ -23,8 +25,9 @@ for _ in range(m):
         for i in range(idx+1, idx+t+1):
             brr[i] = brr[i-1] + 1
     idx = i
+brr[idx+1:] = [brr[idx]] * (len(brr) - idx)
 cnt = 0
-for t in range(1, len(arr)):
+for t in range(1, idx):
     if arr[t-1] != brr[t-1] and arr[t] == brr[t]:
         cnt += 1
 print(cnt)

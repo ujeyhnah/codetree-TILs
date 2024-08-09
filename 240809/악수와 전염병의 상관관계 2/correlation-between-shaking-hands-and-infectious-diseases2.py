4cc7154980899daee86c0d857cd754aedf894190
한혜주 <hyejustudy@gmail.com>
2024-08-09 13:45:54 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240809/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,6 +1,6 @@
 N, K, P, T = map(int, input().split())
-prr = [[0, K] for _ in range(N+1)]
-prr[P] = [1, K]
+prr = [[0, 0] for _ in range(N+1)]
+prr[P] = [1, 0]
 timelist = [[0,0] for _ in range(500)]
 for _ in range(T):
     t, x, y = map(int, input().split())
@@ -8,19 +8,19 @@ for _ in range(T):
 for hand in timelist:
     if hand[0] > 0 and hand[1] > 0:
         x, y = hand[0], hand[1]
-        if prr[x][0] == 1 and (1 <= prr[x][1] <= K):
+        if prr[x][0] == 1 and (0 <= prr[x][1] < K):
             if prr[y][0] == 0:
                 prr[y][0] = 1
             else:
-                prr[y][1] -= 1
-            prr[x][1] -= 1
+                prr[y][1] += 1
+            prr[x][1] += 1
 
-        elif prr[y][0] == 1 and (1 <= prr[y][1] <= K):
+        elif prr[y][0] == 1 and (0 <= prr[y][1] < K):
             if prr[x][0] == 0:
                 prr[x][0] = 1
             else:
-                prr[x][1] -= 1
-            prr[x][1] -= 1
+                prr[x][1] += 1
+            prr[y][1] += 1
 
 for x in range(1, len(prr)):
     print(prr[x][0], end='')

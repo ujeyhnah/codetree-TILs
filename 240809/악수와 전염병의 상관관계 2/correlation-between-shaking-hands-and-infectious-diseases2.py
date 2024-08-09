2f503b1e957de021a74bb80cf92059838c480e96
한혜주 <hyejustudy@gmail.com>
2024-08-09 13:42:59 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240809/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240809/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -9,12 +9,18 @@ for hand in timelist:
     if hand[0] > 0 and hand[1] > 0:
         x, y = hand[0], hand[1]
         if prr[x][0] == 1 and (1 <= prr[x][1] <= K):
-            prr[y][0] = 1
+            if prr[y][0] == 0:
+                prr[y][0] = 1
+            else:
+                prr[y][1] -= 1
             prr[x][1] -= 1
 
-        if prr[y][0] == 1 and (1 <= prr[y][1] <= K):
-            prr[x][0] = 1
-            prr[y][1] -= 1
+        elif prr[y][0] == 1 and (1 <= prr[y][1] <= K):
+            if prr[x][0] == 0:
+                prr[x][0] = 1
+            else:
+                prr[x][1] -= 1
+            prr[x][1] -= 1
 
 for x in range(1, len(prr)):
     print(prr[x][0], end='')

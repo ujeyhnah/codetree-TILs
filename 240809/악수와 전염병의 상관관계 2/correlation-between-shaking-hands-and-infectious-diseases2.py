ed9091b1262f8f16c887cb3916ad6a50a1d7526d
한혜주 <hyejustudy@gmail.com>
2024-08-09 13:27:45 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240809/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240809/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,7 +1,7 @@
 N, K, P, T = map(int, input().split())
 prr = [0] * (N+1)
 prr[P] = 1
-timelist = [[0,0] for _ in range(501)]
+timelist = [[0,0] for _ in range(1001)]
 for _ in range(T):
     t, x, y = map(int, input().split())
     timelist[t] = [x, y]

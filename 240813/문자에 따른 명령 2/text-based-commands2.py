9f384041864d6bd9f29d3014eae91ce1761caf35
한혜주 <hyejustudy@gmail.com>
2024-08-13 16:05:41 +0900

    Title: 문자에 따른 명령 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240813/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,12 @@
+dir_num = 3
+x, y = 0, 0
+dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
+com = input()
+for co in com:
+    if co == 'L':
+        dir_num = (dir_num + 3) % 4
+    elif co == 'R':
+        dir_num을 (dir_num + 1) % 4
+    elif co == 'F':
+        x, y = x + dx[dir_num], y + dy[dir_num]
+print(x, y)

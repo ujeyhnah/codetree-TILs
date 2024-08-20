521f1cabb47a6f0ecfe09becbc7da7f32ed51f72
한혜주 <hyejustudy@gmail.com>
2024-08-20 10:49:13 +0900

    Title: 정수 명령 처리 8, Time: 370ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240820/정수 명령 처리 8/process-numeric-commands-8.py
@@ -0,0 +1,21 @@
+arr = []
+for _ in range(int(input())):
+    co = input().split()
+    if len(co) == 2:
+        a = int(co[1])
+    if co[0] == 'push_front':
+        arr.insert(0, a)
+    elif co[0] == 'push_back':
+        arr.append(a)
+    elif co[0] == 'pop_front':
+        print(arr.pop(0))
+    elif co[0] == 'pop_back':
+        print(arr.pop())
+    elif co[0] == 'size':
+        print(len(arr))
+    elif co[0] == 'empty':
+        print(1 if len(arr) > 1 else 0)
+    elif co[0] == 'front':
+        print(arr[0])
+    elif co[0] == 'back':
+        print(arr[-1])
